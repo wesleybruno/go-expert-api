c944fa2baa1bb7564ec8179ab67ae3a3e5ba08d3
Wesley Oliveira <wesleyoliveira@cronosbank.com.br>
2023-07-17 17:34:17 -0300

    chore: tidy folder and add chi router

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,24 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 	"github.com/wesleybruno/go-expert-api/configs"
-	"github.com/wesleybruno/go-expert-api/internal/dto"
 	"github.com/wesleybruno/go-expert-api/internal/entity"
 	"github.com/wesleybruno/go-expert-api/internal/infra/database"
+	"github.com/wesleybruno/go-expert-api/internal/infra/webserver/handlers"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func main() {
-	configs, err := configs.LoadConfig("./../../")
+	_, err := configs.LoadConfig("./../../")
 	if err != nil {
 		panic(err)
 	}
-	print(configs.DBDriver)
+
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic(err)
@@ -25,51 +26,12 @@ func main() {
 
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 	productDB := database.NewProduct(db)
-	productHandler := NewProductHandler(productDB)
+	productHandler := handlers.NewProductHandler(productDB)
 
-	http.HandleFunc("/products", productHandler.CreateProduct)
-	http.ListenAndServe(":8000", nil)
-}
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 
-type ProductHandler struct {
-	ProductDB database.ProductInterface
-}
+	r.Post("/products", productHandler.CreateProduct)
 
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		msg := struct {
-			Message string `json:"message"`
-		}{
-			Message: err.Error(),
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(msg)
-		return
-	}
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		msg := struct {
-			Message string `json:"message"`
-		}{
-			Message: err.Error(),
-		}
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(msg)
-		return
-	}
-	err = h.ProductDB.Create(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusCreated)
+	http.ListenAndServe(":8000", r)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/wesleybruno/go-expert-api
 go 1.20
 
 require (
+	github.com/go-chi/chi v1.5.1
+	github.com/go-chi/chi/v5 v5.0.10
 	github.com/go-chi/jwtauth v1.2.0
 	github.com/google/uuid v1.1.2
 	github.com/spf13/viper v1.16.0
