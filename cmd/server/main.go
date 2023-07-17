93ac1a7a77d543017f099573ca8ee0635ec1ea58
Wesley Oliveira <wesleyoliveira@cronosbank.com.br>
2023-07-17 16:33:38 -0300

    feat: create product

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,16 @@
 package main
 
-import "github.com/wesleybruno/go-expert-api/configs"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/wesleybruno/go-expert-api/configs"
+	"github.com/wesleybruno/go-expert-api/internal/dto"
+	"github.com/wesleybruno/go-expert-api/internal/entity"
+	"github.com/wesleybruno/go-expert-api/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
 	configs, err := configs.LoadConfig("./../../")
@@ -8,4 +18,58 @@ func main() {
 		panic(err)
 	}
 	print(configs.DBDriver)
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8000", nil)
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: err.Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		msg := struct {
+			Message string `json:"message"`
+		}{
+			Message: err.Error(),
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(msg)
+		return
+	}
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
