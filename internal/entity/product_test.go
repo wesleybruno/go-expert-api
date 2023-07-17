ae2aa0394828f1329812ccd9503cf6261f71046a
Wesley Oliveira <wesleyoliveira@cronosbank.com.br>
2023-07-16 23:17:06 -0300

    feat: create product and his test

---
+++ internal/entity/product_test.go
@@ -0,0 +1,37 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewProduct(t *testing.T) {
+	product, err := NewProduct("Product", 10.0)
+	assert.Nil(t, err)
+	assert.NotNil(t, product)
+	assert.NotEmpty(t, product.ID)
+	assert.NotEmpty(t, product.Name)
+	assert.NotEmpty(t, product.CreatedAt)
+	assert.Equal(t, "Product", product.Name)
+	assert.Equal(t, 10.0, product.Price)
+	assert.Nil(t, product.Validate())
+}
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+	p, err := NewProduct("", 10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrNameIsRequired, err)
+}
+
+func TestProductWhenPriceIsRequired(t *testing.T) {
+	p, err := NewProduct("Product 1", 0)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrPriceIsRequired, err)
+}
+
+func TestProductWhenPriceUnder(t *testing.T) {
+	p, err := NewProduct("Product 1", -10)
+	assert.Nil(t, p)
+	assert.Equal(t, ErrInvalidPrice, err)
+}
