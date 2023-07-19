0ed101bd8c31e3071a8576c2855fb2e49df993de
Wesley Oliveira <wesleyoliveira@cronosbank.com.br>
2023-07-18 21:10:24 -0300

    feat: start doc with swagger

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,7 +6,9 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/jwtauth"
+	httpSwagger "github.com/swaggo/http-swagger"
 	"github.com/wesleybruno/go-expert-api/configs"
+	_ "github.com/wesleybruno/go-expert-api/docs"
 	"github.com/wesleybruno/go-expert-api/internal/entity"
 	"github.com/wesleybruno/go-expert-api/internal/infra/database"
 	"github.com/wesleybruno/go-expert-api/internal/infra/webserver/handlers"
@@ -14,6 +16,20 @@ import (
 	"gorm.io/gorm"
 )
 
+// @title           Go Expert Course - API Example
+// @version         1.0
+// @description     Product API with auhtentication
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   Wesley Oliveira
+// @contact.url    https://www.linkedin.com/in/wesley-oliveira-/
+// @contact.email  wesleybruno.so@gmail.com
+
+// @host      localhost:8000
+// @BasePath  /
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 func main() {
 	configs, err := configs.LoadConfig("./../../")
 	if err != nil {
@@ -50,6 +66,8 @@ func main() {
 
 	r.Post("/users", userHandler.Create)
 	r.Post("/users/generate_token", userHandler.GetJWT)
+
+	r.Get("/docs/*", httpSwagger.Handler(httpSwagger.URL("http://localhost:8000/docs/doc.json")))
 
 	http.ListenAndServe(":8000", r)
 }
